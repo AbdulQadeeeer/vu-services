12a4fb78a1fa9ca7634c633d7c8a8fcd8de154fd
Anonymous <rabdulqadeer96@gmail.com>
2024-01-05 20:41:46 -0500

    add games in home page

---
+++ script/home.js
@@ -0,0 +1,22 @@
+const games = [
+    "./games/js-Planet-Defence/js-Planet-Defence.html",
+    "./games/Snake/Snake.html"
+];
+
+let currentGameIndex = 0;
+
+function showGame(index) {
+    const iframe = document.getElementById('currentGame');
+    iframe.src = games[index];
+    currentGameIndex = index;
+}
+
+function showPreviousGame() {
+    currentGameIndex = (currentGameIndex - 1 + games.length) % games.length;
+    showGame(currentGameIndex);
+}
+
+function showNextGame() {
+    currentGameIndex = (currentGameIndex + 1) % games.length;
+    showGame(currentGameIndex);
+}
